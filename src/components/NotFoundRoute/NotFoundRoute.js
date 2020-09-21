89504edb071a773b25f47b3b5ea4abd0f24bbe54
Alejandro <alejocano22@gmail.com>
2020-09-21 16:20:47 -0500

    Page Not Found

--- src/components/NotFoundRoute/NotFoundRoute.js
+++ src/components/NotFoundRoute/NotFoundRoute.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import NotFoundImage from '../../images/psyduck.png';
+import styles from './notFoundRoute.module.css';
 
 const NotFoundRoute = () =>{
   return(
-    <div>
-      <h1>404 not foud!</h1>
+    <div className={styles['not-found-container']}>
+      <img
+        src={NotFoundImage}
+        alt='Not found'
+        className={styles['not-found-image']}
+      ></img>
+      <h1 className={styles['not-found-message']}>PAGE NOT FOUND!</h1>
+      <NavLink to='/' className={styles['back-home']}>Back Home</NavLink>
     </div>
   )
 }
