3901a76ae9d03979cca1250f6b3002ae19265292
Alejandro <alejocano22@gmail.com>
2020-09-22 18:34:45 -0500

    comparison card

--- src/components/PokemonComparison/PokemonComparison.js
+++ src/components/PokemonComparison/PokemonComparison.js
@@ -1,52 +1,83 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-import { closeComparisonPokemonModal } from '../../redux/actions/pokemonComparisonActions'
-import { isNotComparing } from '../../redux/actions/pokemonCardActions'
+import { closeComparisonPokemonModal } from '../../redux/actions/pokemonComparisonActions';
+import { isNotComparing } from '../../redux/actions/pokemonCardActions';
+import { pokemonImagesUrl } from '../../utils';
+import Chart from '../Chart';
 import styles from './pokemonComparison.module.css';
 
 const PokemonComparison = (props) =>{
 
-  const handleClose = () => {
+  const handleCloseModal = () => {
     props.closeComparisonPokemonModal();
     props.isNotComparing();
   }
 
-  const handleProp = (event) => {
+  const handlePropagation = (event) => {
     event.stopPropagation();
   }
 
+
   if(props.comparisonPokemon.isActive){
+    let gender = 'Genderless';
+    if(props.currentPokemon.genderRate >= 4 ){
+      gender = 'Female';
+    }else if (props.currentPokemon.genderRate >= 0){
+      gender = 'Male';
+    }
     return(  
-      <div className={styles['modal']} onClick={handleClose}>
-        <div className={styles['card']} onClick={handleProp}>
-          <h1 className={styles['card-title']}>{props.currentPokemon.name.toUpperCase()}</h1>
-          <div className='img-comp'>
-            <img
-              className={styles['card-image']}
-              src= {"https://github.com/PokeAPI/sprites/blob/146c91287ad01f6e15315bbd733fd7442c91fe6d/sprites/pokemon/"+(props.currentPokemon.id)+".png?raw=true"}
-              alt={props.currentPokemon.id}>
-            </img> 
-            <h1>Vs</h1>
-            <img
-              className={styles['card-image']}
-              src= {"https://github.com/PokeAPI/sprites/blob/146c91287ad01f6e15315bbd733fd7442c91fe6d/sprites/pokemon/"+(props.comparisonPokemon.id)+".png?raw=true"}
-              alt={props.currentPokemon.id}>
-            </img> 
+      <div className={styles['comparison-modal']} onClick={handleCloseModal}>
+        <div className={styles['comparison-card']} onClick={handlePropagation}>
+          <div className={styles['pokemon-header']}>
+            <button className={styles['close-button']}onClick={handleCloseModal}>X</button>
+            <h2 className={styles['pokemon-name']}>{props.currentPokemon.name.toUpperCase()} vs. {props.comparisonPokemon.name.toUpperCase()}</h2>
           </div>
-          
-          <h1 className={styles['card-title']}>{props.comparisonPokemon.name.toUpperCase()}</h1>
+          <div className={styles['pokemon-information']}>
+            <div className={styles['pokemon-images']}>
+              <img
+                className={styles['card-image']}
+                src= {pokemonImagesUrl+(props.currentPokemon.id)+".png?raw=true"}
+                alt={props.currentPokemon.id}>
+              </img> 
+              <img
+                className={styles['card-image']}
+                src= {pokemonImagesUrl+(props.comparisonPokemon.id)+".png?raw=true"}
+                alt={props.currentPokemon.id}>
+              </img>
+            </div>
+            <div className={styles['pokemon-details']}>
+              <h4 className={styles['pokemon-detail-description']}>{gender}</h4>
+              <h3 className={styles['pokemon-detail-title']}>Gender</h3>
+              <h4 className={styles['pokemon-detail-description']}>{gender}</h4>
+              <h4 className={styles['pokemon-detail-description']}>{props.currentPokemon.height}</h4>
+              <h3 className={styles['pokemon-detail-title']}>Height</h3>
+              <h4 className={styles['pokemon-detail-description']}>{props.currentPokemon.height}</h4>  
+              <h4 className={styles['pokemon-detail-description']}>{props.currentPokemon.weight}</h4>
+              <h3 className={styles['pokemon-detail-title']}>Weight</h3>
+              <h4 className={styles['pokemon-detail-description']}>{props.currentPokemon.weight}</h4>
+              <ul className={styles['pokemon-detail-list']}> 
+                {props.currentPokemon.abilities.map((ability, index)=>(
+                  <li className={styles['pokemon-detail-description']} key={index}>{ability.ability.name}</li>
+                ))}
+              </ul>
+              <h3 className={styles['pokemon-detail-title']}>Abilities</h3>
+              <ul className={styles['pokemon-detail-list']}> 
+                {props.currentPokemon.abilities.map((ability, index)=>(
+                  <li className={styles['pokemon-detail-description']} key={index}>{ability.ability.name}</li>
+                ))}
+              </ul>   
+            </div>
+          </div> 
+          <div className={styles['pokemon-stats']}>
+            <Chart/>
+          </div> 
         </div>
       </div>
     )
   }else{
     return null;
   }
-
-
-
 }
-
 
 const mapStateToProps = (state) => {
   return state;
