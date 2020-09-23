fbf99d2413dff147d86c2023826d199159fd4326
Alejandro <alejocano22@gmail.com>
2020-09-23 18:16:11 -0500

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, HashRouter } from 'react-router-dom';
 import store from './redux/store';
 import Welcome from './components/Welcome';
 import PokemonList from './components/PokemonList';
@@ -8,12 +8,12 @@ import PageNotFound from './components/PageNotFound';
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
+    <BrowserRouter basename='/'>
+      <HashRouter>
         <Route path='/' exact component={Welcome}></Route>
         <Route path='/pokemon' component={PokemonList}></Route>
         <Route path='*' exact={true} component={PageNotFound} />
-      </Switch>
+      </HashRouter>
     </BrowserRouter>
   </Provider>
 );
