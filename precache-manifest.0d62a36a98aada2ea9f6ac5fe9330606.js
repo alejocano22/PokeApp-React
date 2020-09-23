3177ea06e8da73ffdf29f1e5078f6f1e663cbd1c
Alejandro <alejocano22@gmail.com>
2020-09-23 15:29:32 -0500

    Updates

---
+++ precache-manifest.0d62a36a98aada2ea9f6ac5fe9330606.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3377727cfe9f6455e328cc0b07cf27",
+    "url": "/pokeapp-react/index.html"
+  },
+  {
+    "revision": "0e4b9cc72abf3535c1cd",
+    "url": "/pokeapp-react/static/css/main.2fb8af1b.chunk.css"
+  },
+  {
+    "revision": "e7cca0785eede2e68cb6",
+    "url": "/pokeapp-react/static/js/2.d4d9d531.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/pokeapp-react/static/js/2.d4d9d531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4b9cc72abf3535c1cd",
+    "url": "/pokeapp-react/static/js/main.4234e2e4.chunk.js"
+  },
+  {
+    "revision": "07757be8237a7874f696",
+    "url": "/pokeapp-react/static/js/runtime-main.81cd5510.js"
+  },
+  {
+    "revision": "01b24f9949b34b05ad1992711b05a26d",
+    "url": "/pokeapp-react/static/media/pokeBackground.01b24f99.png"
+  },
+  {
+    "revision": "38c6e1a50d33f6c0c1e09eced4229b94",
+    "url": "/pokeapp-react/static/media/psyduck.38c6e1a5.png"
+  }
+]);
